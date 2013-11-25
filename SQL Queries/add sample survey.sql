1ec7cfd05334d7838bbd813995cbb0d7a3f0c781
Josh <javw.phanatic@gmail.com>
2013-11-24 19:10:05 -0500

    outdated/unneccessary

--- SQL Queries/add sample survey.sql
+++
@@ -1,9 +0,0 @@
-insert into Survey (Name) values ('Sample Survey');
-insert into Question (Question, Survey_id, Type_id) values ('Name?', 1, 1);
-insert into Question (Question, Survey_id, Type_id) values ('Gender?', 1, 2);
-insert into Question (Question, Survey_id, Type_id) values ('Age?', 1, 1);
-insert into Question (Question, Survey_id, Type_id) values ('Location', 1, 1);
-insert into Options (Question_id, Option_text) values (2, 'male');
-insert into Options (Question_id, Option_text) values (2, 'female');
-insert into Type (Name) values ('textbox');
-insert into Type (Name) values ('radio');
